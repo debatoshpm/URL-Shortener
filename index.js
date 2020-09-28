15e2cb875967ac1e6b5e08a54d591c3eb3ce545a
Debatosh Pal Majumder <36793454+debatoshpm@users.noreply.github.com>
2020-09-28 11:33:14 +0530

    bug fix

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ mongoose
   .then(() => console.log("MongoDB connected..........."))
   .catch((err) => console.log(err));
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use("/", index);
 app.use("/api/url", url);
